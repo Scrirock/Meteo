849ecc647aad8e169364aa1fa8f5b2b346010e54
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-13 10:18:26 +0100

    jquerry

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,27 @@
-document.getElementById("go").addEventListener("click", ()=>{
-    let city = document.getElementById("city");
+$("#go").click(function (){
+    let city = $("#city");
     meteo(city);
 });
 
-
-
 function meteo(city){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&units=metric&lang=fr&appid=a60f16c1bbd0df1c3be7109d59a3f46f";
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = "json";
-
-    xhr.onload = () => {
-        if (xhr.status !== 200) {
-            return;
-        }
-
-        console.log(xhr.response)
-        document.getElementById("ville").innerHTML = "Ville: " + xhr.response.name;
-        document.getElementById("co").innerHTML = "Latitude: " + xhr.response.coord.lat + " / Longitude: " + xhr.response.coord.lon;
-        document.getElementById("country").innerHTML = "Pays: " + xhr.response.sys.country
-        document.getElementById("temperature").innerHTML = "Température: " + xhr.response.main.temp + " degrés Celsius <--> ";
-        document.getElementById("feelsLike").innerHTML = "Ressentis: " + xhr.response.main.feels_like + " degrés Celsius";
-        document.getElementById("min").innerHTML = " Min: " + xhr.response.main.temp_min + " degrés Celsius <--> ";
-        document.getElementById("max").innerHTML = "Max: " + xhr.response.main.temp_max + " degrés Celsius";
-        document.getElementById("humidity").innerHTML = "Humiditée: " + xhr.response.main.humidity + " %";
-        document.getElementById("pressure").innerHTML = "Pression: " + xhr.response.main.pressure;
-        document.getElementById("description").innerHTML = "Description: " + xhr.response.weather[0].description;
-        document.getElementById("degree").innerHTML = "Angle du vent: " + xhr.response.wind.deg;
-        document.getElementById("speed").innerHTML = "Vitesse du vent: " + xhr.response.wind.speed + " km/h";
-    }
-
-    xhr.send();
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city.val() + "&units=metric&lang=fr&appid=a60f16c1bbd0df1c3be7109d59a3f46f";
+    $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json"
+    })
+    .done (function (response){
+        $("#ville").html("Ville: " + response.name);
+        $("#co").html("Latitude: " + response.coord.lat + " / Longitude: " + response.coord.lon);
+        $("#country").html("Pays: " + response.sys.country);
+        $("#temperature").html("Température: " + response.main.temp + " degrés Celsius <--> ");
+        $("#feelsLike").html("Ressentis: " + response.main.feels_like + " degrés Celsius");
+        $("#min").html(" Min: " + response.main.temp_min + " degrés Celsius <--> ");
+        $("#max").html("Max: " + response.main.temp_max + " degrés Celsius");
+        $("#humidity").html("Humiditée: " + response.main.humidity + " %");
+        $("#pressure").html("Pression: " + response.main.pressure);
+        $("#description").html("Description: " + response.weather[0].description);
+        $("#degree").html("Angle du vent: " + response.wind.deg);
+        $("#speed").html("Vitesse du vent: " + response.wind.speed + " km/h");
+    })
 }
